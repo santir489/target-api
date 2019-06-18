cee4701bed9a963d3d9c9bc71ba408746ccd371c
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2019-06-18 11:58:29 -0300

    Add confirm account email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable,
+        :confirmable
   include DeviseTokenAuth::Concerns::User
 
   enum gender: { female: 0, male: 1 }
--- db/migrate/20190612144842_add_user_attributes.rb
+++ db/migrate/20190612144842_add_user_attributes.rb
@@ -1,8 +1,8 @@
 class AddUserAttributes < ActiveRecord::Migration[5.2]
   def change
-    add_column          :users, :gender, :integer 
-    change_column_null  :users, :gender, false   
+    add_column          :users, :gender, :integer
+    change_column_null  :users, :gender, false
     change_column_null  :users, :name, false
-    change_column_null  :users, :email, false 
+    change_column_null  :users, :email, false
   end
 end
