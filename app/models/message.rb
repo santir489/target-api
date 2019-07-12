d5941964ddaa5fdab10f43251db2f9628ea0eb3a
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2019-07-12 14:07:08 -0300

    feature/chat-part2 validate user message to belgon conversation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,16 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
-  validates :text, :conversation, :user, presence: true
+  validates :text, presence: true
+  validate  :user_in_conversation, on: :create
 
   after_create_commit { MessageBroadcastJob.perform_later(self) }
+
+  private
+
+  def user_in_conversation
+    return if conversation.users.include?(user)
+
+    errors.add(:user_in_conversation, I18n.t('api.errors.user_not_conversation'))
+  end
 end
--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
-    text { Faker::Lorem.sentence(1, true, 6) }
-    conversation
+    text         { Faker::Lorem.sentence(1, true, 6) }
+    conversation { create(:convarsation_with_users, user1: user) }
     user
   end
 end
