ec74b90004f1a96e47b0e9660d064680d2c45c6e
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2019-07-12 14:29:03 -0300

    feature/chat-part2 test NotifyMessageJob

--- app/models/conversations_user.rb
+++ app/models/conversations_user.rb
@@ -2,8 +2,6 @@ class ConversationsUser < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
-  validates :user, :conversation, presence: true
-
   def online
     update(connected: true)
   end
